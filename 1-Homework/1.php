f339651aeda348c18a0d278c44bf55a30c810e15
Mikhail <m.chuenkov@gmail.com>
2018-09-08 22:07:54 +0300

    Выполнил первое домашнее задание

--- 1-Homework/1.php
+++ 1-Homework/1.php
@@ -3,5 +3,25 @@
  * Created by PhpStorm.
  * User: Питекантроп
  * Date: 08.09.2018
- * Time: 20:16
+ * Time: 21:11
  */
+
+$name = "Михаил";
+$age = 32;
+$oneYear = 1;
+$agePlus1 = $age + 1;
+$agePlus2 = $age + 2;
+$day = date(d);
+$month = date(F);
+$year = date(Y);
+$hoer = date(G);
+$minute = date(i);
+$time = "$day-$month-$year $hoer:$minute";
+$data = "Меня зовут $name. Мне $age года. Через год мне будет {$agePlus1}. А еще через год - {$agePlus2} На моих 
+часах $time";
+
+echo "<p>$data</p>";
+echo str_replace(" ", "_", $data);
+echo "<br><br>";
+echo stristr($data, "На");
+
---
+++ 1-Homework/2.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Питекантроп
+ * Date: 08.09.2018
+ * Time: 21:11
+ */
+header('Location: http://1-homework-php/1.php');

43d558a2dd8a0cd4f06d2f8dbcb81753da95090e
Mikhail <m.chuenkov@gmail.com>
2018-10-03 00:36:16 +0300

    Сделал корзину. Задание 1

--- 7-Homework/engine/goods.php
+++ 7-Homework/engine/goods.php
@@ -9,6 +9,16 @@ function getProduct($id){
     $sql = "SELECT * FROM goodsData WHERE id = '{$id}'";
     return queryOne($sql);
 }
+function getProductsFromBasket(){
+    getConnection();
+    $sql = "SELECT * FROM basket";
+    return queryAll($sql);
+}
+function getProductsFromBasketById($id){
+    getConnection();
+    $sql = "SELECT * FROM basket WHERE productId = '{$id}'";
+    return queryOne($sql);
+}
 
 function render($template, $parameters = []){
     extract($parameters);
--- 7-Homework/public/index.php
+++ 7-Homework/public/index.php
@@ -6,6 +6,7 @@ include ENGINE_DIR . "base.php";
 include ENGINE_DIR . "goods.php";
 include ENGINE_DIR . 'db.php';
 include ENGINE_DIR . 'users.php';
+include ENGINE_DIR . 'basket.php';
 
 $res1 = mathOperation($_GET['num1'],$_GET['num2'], $_GET['operation']);
 $res2 = mathOperation($_POST['num1'],$_POST['num2'], $_POST['operation']);
@@ -40,13 +41,32 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
             redirect("/myofficeerror.php");
         }
     }
+    if($_POST['buybtn']){
+        $buyProductId = $_POST['buybtn'];
+        $getProduct = getProduct($buyProductId);
+        if (getProductsFromBasketById($buyProductId)) {
+            updateProductToBasket($buyProductId);
+        } else {
+            $addProductToBasket = addProductToBasket($getProduct);
+            redirect("/index.php");
+        }
+    }
+    if($_POST['delProduct']){
+        $delProductId = $_POST['delProduct'];
+        $delProduct = delProduct($delProductId);
+        redirect("/index.php");
+    }
 }
-
+$getSummBasketByProduct = getSummBasketByProduct();
+$getSummBasket = getSummBasket($getSummBasketByProduct);
 $getComments = getComments();
 $getGoods = getGoods();
+$getProductsFromBasket = getProductsFromBasket();
 $parameters = [
     'getGoods' => $getGoods,
     'getComments' => $getComments,
+    'getProductsFromBasket' => $getProductsFromBasket,
+    'getSummBasket' => $getSummBasket,
 ];
 render('products', $parameters);
 
